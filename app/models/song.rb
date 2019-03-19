680c34f458dc6b7a0979b24457bd1239ec1ad428
loganwohlers <lawohlers@gmail.com>
2019-03-19 11:13:55 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def artist_view
+    "#{self.artist_name} - #{self.title}"
+  end
 end
