277a1a1e233028d589f062cc2973c2fcf24bfd75
github username <github email address>
2019-03-19 17:37:51 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+after_create :song_count
+
+  def song_count
+    self.songs.count
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+mogli = Artist.create!(name: "Mogli")
+maps = Artist.create!(name: "Maps")
+
+maps.songs.create!(title: "Aint no body got enough time!")
+maps.songs.create!(title: "Why does the sun hurt so good?")
+mogli.songs.create!(title: "Fire in tha valley")
+mogli.songs.create!(title: "Fermented Apple Water")
+# clickbait = Category.create!(name: "Motivation")
+# clickbait.posts.create!(title: "10 Ways You Are Already Awesome")
+# clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
+# clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
