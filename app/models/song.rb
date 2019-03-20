6ac430f34531e921e1075f5b354faff0874da287
Reid Boudreau <rtboudreau@gmail.com>
2019-03-19 20:30:21 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+      self.artist.name if self.artist
+  end
+
+  def format_with_artist
+    "#{self.artist.name} - #{self.title}"
+  end
+
 end
